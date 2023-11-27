d0975910d434cd5086642f512c18367850b1342b
tringuyen99 <tringuyen9685@gmail.com>
2023-11-27 17:00:26 +1100

    Initial Commit

---
+++ brocckiethewise/brocckiethewiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  brocckiethewiseApp.swift
+//  brocckiethewise
+//
+//  Created by Tri nguyen on 27/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct brocckiethewiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
